906af89d5538059704838dbe6ac37716ba5964c4
yara <yarysalinas@gmail.com>
2024-04-12 12:44:34 +0200

    Ascii

--- ft_isascii.c
+++ ft_isascii.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_ isascii.c                                      :+:      :+:    :+:   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*   By: yadiaman <yadiaman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/11 12:52:55 by marvin            #+#    #+#             */
-/*   Updated: 2024/04/11 12:52:55 by marvin           ###   ########.fr       */
+/*   Created: 2024/04/12 12:30:35 by yadiaman          #+#    #+#             */
+/*   Updated: 2024/04/12 12:43:30 by yadiaman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,10 @@ int ft_isascii(int c)
         return(0);
 }
 
-/*#include <stdio.h>
+/* #include <stdio.h>
 int main(void)
 {
-    char c = 'Å';
+    char c = 'ƒ';
     printf("%d", ft_isascii(c));
     return(0);
 }
