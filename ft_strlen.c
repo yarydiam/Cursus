d25c2077492df806861444960780223eb84a8c73
yara <yarysalinas@gmail.com>
2024-04-14 17:09:18 +0200

    Actualizados

--- ft_strlen.c
+++ ft_strlen.c
@@ -5,32 +5,28 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: yadiaman <yadiaman@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/12 14:08:13 by yadiaman          #+#    #+#             */
-/*   Updated: 2024/04/14 15:20:20 by yadiaman         ###   ########.fr       */
+/*   Created: 2024/04/14 16:55:48 by yadiaman          #+#    #+#             */
+/*   Updated: 2024/04/14 17:08:12 by yadiaman         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include <stdio.h>
 
-size_t ft_strlen(const char *str)
+size_t	ft_strlen(const char *str)
 {
-    size_t i;
+	size_t	i;
 
-    i = 0;
-
-    while(str[i] != '\0')
-    {
-        i++;   
-    }
-    return(i);   
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
 }
 
-
-/* int main()
+/* int	main(void)
 {
-    const char *cadena1 = "Que pasa tronco";
+	const	char *cadena1 = "Que pasa tronco";
 
-    printf("Longitud de cadena1: %zu", ft_strlen(cadena1));
-}
-*/
+	printf("Longitud de cadena1: %zu", ft_strlen(cadena1));
+} */
